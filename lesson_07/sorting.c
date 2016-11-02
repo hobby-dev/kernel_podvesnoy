8c0a17a2fb43a5db6dd008864eef12abb104ead1
hobby-dev <hobby-dev@users.noreply.github.com>
2016-11-02 20:22:20 +0300

    + sorting

---
+++ lesson_07/sorting.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+#define SIZE 5 
+
+void swap(int data[], int from, int to)
+{
+    if (from != to)
+    {
+        int temporary = data[from];
+        data[from] = data[to];
+        data[to] = temporary;
+    }
+}
+
+void sort(int data[], int size)
+{
+    int first_unsorted_index = 0;
+    const int last_index = size - 1;
+    // loop invariant: there is always at least 'first_unsorted' elements in data[]
+    while (first_unsorted_index < last_index)
+    {
+        int current_minimal_index = first_unsorted_index;
+        for (int i = current_minimal_index; i < size; ++i)
+        {
+            if (data[i] < data[current_minimal_index])
+                current_minimal_index = i;
+        }
+        swap(data, current_minimal_index, first_unsorted_index);
+        ++first_unsorted_index;
+    }
+}
+
+int main()
+{
+    int data[SIZE] = { 0 };
+    printf("Enter %d elements:\n", SIZE);
+    for (int i = 0; i < SIZE; ++i)
+    {
+        scanf("%d", &data[i]);
+    }
+    sort(data, SIZE);
+
+    for (int i = 0; i < SIZE; ++i)
+    {
+        printf("%d, ", data[i]);
+    }
+    printf("\n");
+    return 0;
+}
