c07e9f099e47ca7c2956c5b48d03e44cbef1bc5a
hobby-dev <hobby-dev@users.noreply.github.com>
2016-12-13 00:04:20 +0300

    binary write

---
+++ lesson_11/binary_write.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "person_struct.h"
+
+#define PERSONS_COUNT 3
+
+int main()
+{
+    int size = PERSONS_COUNT;
+
+    struct person persons[PERSONS_COUNT] = {
+        { "Vasya", 23, 4.0 },
+        { "Petya", 24, 4.1 },
+        { "Kolya", 25, 4.2 }
+    };
+
+    FILE * out = fopen("persons.dat", "w");
+    if (out != NULL)
+    {
+        int ret = fwrite(&size, sizeof(size), 1, out);
+        if (ret > 0)
+        {
+            printf("size written\n");
+            ret = fwrite(persons, sizeof(struct person), size, out);
+            if (ret == size)
+                printf("persons written\n");
+            else
+                perror("fwrite");
+        }
+        else
+            perror("fwrite");
+    }
+    else
+        perror("fopen");
+
+    return 0;
+}
---
+++ lesson_11/person_struct.h
@@ -0,0 +1,7 @@
+#define NAME_LEN 32
+struct person
+{
+    char name[NAME_LEN];
+    int age;
+    double average_mark;
+};
