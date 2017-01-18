6a72549c8b10733e30c2f593f250efc07857918a
hobby-dev <hobby-dev@users.noreply.github.com>
2017-01-18 20:54:56 +0300

    client & server

---
+++ lesson_16/client.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <strings.h>
+#include <unistd.h> // write()
+
+int main()
+{
+    int client_socket = socket( AF_UNIX, SOCK_STREAM, 0);
+
+    if (client_socket < 0)
+    {
+        perror("socket");
+        exit(1);
+    }
+
+    struct sockaddr_un server_address = {AF_UNIX, "/tmp/my_socket"};
+    
+    int ret = connect(client_socket, (struct sockaddr*)&server_address, SUN_LEN(&server_address));
+
+    if (ret < 0)
+    {
+        perror("connect");
+        exit(1);
+    }
+
+    char message_to_server[10] = "hello";
+    int written_bytes = write(client_socket, message_to_server, strlen(message_to_server));
+    if (written_bytes <= 0)
+    {
+        perror("write");
+        exit(1);
+    }
+    printf("Message sent\n");
+
+    close(client_socket);
+
+
+    return 0;
+}
---
+++ lesson_16/server.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h> // exit();
+#include <sys/socket.h>
+#include <sys/un.h> // sockaddr_un
+#include <unistd.h> // read?
+// #include <sys/types.h> // sockaddr?
+
+#define MAX_CLIENTS 100500
+
+int main()
+{
+    unlink("/tmp/my_socket");
+    int sock_fd = 0;
+
+    sock_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+
+    struct sockaddr_un our_addr = {AF_UNIX, "/tmp/my_socket"};
+    
+    int ret = bind( sock_fd, (struct sockaddr*)&our_addr, SUN_LEN(&our_addr)); 
+    if (ret < 0)
+    {
+        perror("bind");
+    };
+
+    ret = listen(sock_fd, MAX_CLIENTS);
+
+    struct sockaddr_un client_info;
+    int client_info_len;
+
+    int client_fd = accept(sock_fd, (struct sockaddr *)&client_info, &client_info_len);
+
+    char message[10] = {0};
+    ret = read(client_fd, message, 10);
+
+    if (ret < 0)
+    {
+        perror("read");
+        exit(1);
+    }
+
+    printf("Message received: %s\n", message);
+
+    close(client_fd);
+    close(sock_fd);
+
+    return 0;
+}
