168f73b67307771e4ba148d3ab7ed94e37b98c72
hobby-dev <hobby-dev@users.noreply.github.com>
2017-05-25 23:02:20 +0300

    алгоритм планирования

--- lesson_30/sched.c
+++ lesson_30/sched.c
@@ -1,38 +1,54 @@
 #include <stdio.h>
+#include <malloc.h>
 
 struct process
 {
+    int id;
     int priority;
     int time_to_complete;
     int age;
+    // statistics:
     int total_time_waited;
     int total_time;
-
+    // list:
     struct process * next;
-    struct process * prev;
 };
 
 int print_process_list(struct process * head)
 {
+    while(head != NULL)
+    {
+        //printf("P: %d; TC: %d\n", head->priority, head->time_to_complete);
+        printf("id: %d, total_time: %d, total_time_waited: %d\n", head->id, head->total_time, head->total_time_waited);
+        head = head->next;
+    }
     return 0;
 };
 
-int create_process_list_from_file(const char * filename, struct process * head)
+int create_process_list_from_file(const char * filename, struct process ** head)
 {
     FILE *input_file;
     input_file = fopen(filename, "r");
     if (input_file)
     {
         int ret;
+        int id = 0;
         do {
             int new_process_priority = 0;
             int new_process_complete_time = 0;
 
             ret = fscanf(input_file, "P:%d;TC:%d\n", &new_process_priority, &new_process_complete_time);
-            if (ret)
+            if (ret != EOF)
             {
-                printf("priority %d, time to complete: %d\n", new_process_priority, new_process_complete_time);
+                struct process * current = (struct process*)malloc(sizeof(struct process));
+                current->id = id;
+                current->priority = new_process_priority;
+                current->time_to_complete = new_process_complete_time;
+                current->next = NULL;
+                *head = current;
+                head = &current->next;
             }
+            ++id;
         } while (ret != EOF);
     }
     else
@@ -40,14 +56,52 @@ int create_process_list_from_file(const char * filename, struct process * head)
     return 0;
 };
 
+void schedule(struct process ** head, struct process ** completed)
+{
+    struct process * first = *head;
+    first->total_time += 1;
+
+    struct process * last = *head;
+    while(last->next != NULL)
+    {
+        last = last->next;
+        last->age += 1;
+        last->total_time_waited += 1;
+        last->total_time += 1;
+    }
+
+    first->age = 0;
+    first->time_to_complete -= 1;
+
+    if (first->time_to_complete == 0)
+    {
+        *head = first->next;
+        first->next = *completed;
+        *completed = first;
+    }
+    else if (first != last)
+    {
+        *head = first->next;
+        last->next = first;
+        first->next = NULL;
+    }
+
+};
+
 int main()
 {
-    struct process* all_processes;
-    int ret = create_process_list_from_file("all_processes.txt", all_processes);
+    struct process * completed_head = NULL;
+    struct process * active_head = NULL;
+
+    int ret = create_process_list_from_file("all_processes.txt", &active_head);
     if (ret != 0)
         perror("Create process list");
 
-    print_process_list(all_processes);
+    while (active_head != NULL)
+    {
+        schedule(&active_head, &completed_head);
+    }
+    print_process_list(completed_head);
 
     return 0;
-}
+};
